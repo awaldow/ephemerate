5b82a69b7ccda12fc26227b10204012c71720f44
Addison Waldow <a.wal.bear@gmail.com>
2021-12-12 18:09:14 -0700

    update local files for testing helm chart with local container against local/any other cluster

--- local/helmTestDeploy.sh
+++ local/helmTestDeploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm upgrade --install -f values.yaml ephemerate tessellate/ephemerate --version 0.0.1-v1alpha1
+helm upgrade --install -f values.yaml ephemerate tessellate/ephemerate --version 0.0.2
