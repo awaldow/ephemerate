6bf36bf404f5026a7ea72d2d8a13d30c7de1dfc3
Addison Waldow <a.wal.bear@gmail.com>
2021-12-27 13:43:53 -0700

    adjust Dockerfile to run as non-root user

--- local/helmTestUninstall.sh
+++ local/helmTestUninstall.sh
@@ -4,5 +4,5 @@ kubectl patch ephemerators.tessellate.io ephemerator-deleted -p '{"metadata": {"
 kubectl patch ephemerators.tessellate.io ephemerator-disabled -p '{"metadata": {"finalizers": []}}' --type merge
 kubectl patch ephemerators.tessellate.io ephemerator-ignored -p '{"metadata": {"finalizers": []}}' --type merge
 kubectl patch ephemerators.tessellate.io ephemerator-until -p '{"metadata": {"finalizers": []}}' --type merge
-kubectl patch ephemerators.tessellate.io ephemerator-deleted -p '{"metadata": {"finalizers": []}}' --type merge
+
 helm uninstall ephemerate
